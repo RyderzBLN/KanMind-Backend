117142996aa174cf4cbd27594da02f06ab1d758b
Murat Catili <158097420+RyderzBLN@users.noreply.github.com>
2025-07-04 23:38:19 +0200

    init

---
+++ user_auth_app/api/serializers.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    repeated_password = serializers.CharField(write_only=True)  
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'repeated_password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def validated_email(self, value):
+
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError('Email already exists')
+        return value
+
+    def save(self):
+        pw = self.validated_data['password']
+        repeated_pw = self.validated_data['repeated_password']
+
+
+        if pw != repeated_pw:
+            raise serializers.ValidationError({'error': 'Passwords don’t match'})
+
+
+        account = User(
+            email=self.validated_data['email'],
+            username=self.validated_data['username']
+        )
+        account.set_password(pw) 
+        account.save()
+        return account
