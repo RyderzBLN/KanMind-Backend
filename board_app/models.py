648b7effaddf5abb40b14f2d3d9c4f2f05c2364e
Murat Catili <158097420+RyderzBLN@users.noreply.github.com>
2025-07-28 00:06:33 +0200

    change in models
    - for create superuser ( user field )

--- board_app/models.py
+++ board_app/models.py
@@ -9,9 +9,9 @@ class Board(models.Model):
         on_delete=models.CASCADE,
         related_name='owned_boards'
     )
+
     members = models.ManyToManyField(
         settings.AUTH_USER_MODEL)
-
 
     def __str__(self):
         return self.title
@@ -23,27 +23,30 @@ class Board(models.Model):
 
 class Ticket(models.Model):
     PRIORITY_CHOICES = [
-        ('low', 'Low'),
-        ('medium', 'Medium'),
-        ('high', 'High'),
-    ]
+    ('low', 'low'),
+     ('medium', 'medium'),
+    ('high', 'high'),
+]
 
     board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='tickets')
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     status = models.CharField(
     max_length=20,
+
     choices=[
-        ('to_do', 'To Do'),
-        ('in_progress', 'In Progress'),
-        ('done', 'Done'),
+        ('to_do', 'to Do'),
+        ('in_progress', 'in Progress'),
+        ('done', 'done'),
     ],
 )
+    
     priority = models.CharField(
         max_length=10,
         choices=PRIORITY_CHOICES,
         default='medium'
     )
+
     created_at = models.DateTimeField(auto_now_add=True)
     
     assignee = models.ForeignKey(
@@ -53,6 +56,7 @@ class Ticket(models.Model):
         blank=True,
         related_name='assigned_tickets'
     )
+
     reviewer = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
@@ -60,6 +64,7 @@ class Ticket(models.Model):
         blank=True,
         related_name='reviewed_tickets'
     )
+
     due_date = models.DateField(null=True, blank=True)
     comments_count = models.IntegerField(default=0)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name="created_tickets")
@@ -70,11 +75,6 @@ class Ticket(models.Model):
     class Meta:
         verbose_name = 'Ticket'
         verbose_name_plural = 'Tickets'
-
-
-
-
-
 
 
 class Comment(models.Model):
--- user_auth_app/models.py
+++ user_auth_app/models.py
@@ -1,16 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-
 
 class KanbanUser(AbstractUser):
     fullname = models.CharField(max_length=150, unique=False)
     email = models.EmailField(unique=True)
 
-
     USERNAME_FIELD = 'email'  # 🔑 Login erfolgt mit Email!
-    REQUIRED_FIELDS = ['fullname']  # Wird bei createsuperuser abgefragt
+    REQUIRED_FIELDS = ['username']  # Wird bei createsuperuser abgefragt
    
 
     def __str__(self):
